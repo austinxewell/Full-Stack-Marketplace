9908e0a2af77eacbf459a8edb7688ec5ccabf421
AustinxEwell <austin.ewell86@gmail.com>
2021-10-31 09:14:42 -0600

    created links between pages of the website and got the login/signup function started

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -49,9 +49,6 @@ async function signupFormHandler(event) {
     }
 }
 
-document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+document.querySelector('#login-form').addEventListener('Login', loginFormHandler);
+
+document.querySelector('#signup-form').addEventListener('Sign Up', signupFormHandler);
