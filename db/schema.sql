6a4ec357f910f559f885493fb115e867e23d95b7
AustinxEwell <austin.ewell86@gmail.com>
2021-11-01 22:39:29 -0600

    added some css, changed shipping option on my listing to drop down with true or false values.

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
 DROP DATABASE IF EXISTS fullstack_db;
 
 CREATE DATABASE fullstack_db;
-
+USE fullstack_db;
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -24,4 +24,4 @@ async function editFormHandler(event) {
   
   }
   
-  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  document.querySelector('#edit-post-form').addEventListener('submit', editFormHandler);
