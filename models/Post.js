2997018bc02283cb28ddaa5b08d5ae1eeaa6a593
JoseEPina <jose.edpina@gmail.com>
2021-11-02 00:22:38 -0600

    Added npm express-fileupload package. Updated Server.js to import express-fileupload and added middleware. Modified edit-post.hbs to replace temp code with working code. Added Form processing for simultaneous file & fields upload. Replaced eventListener. Updated post-routes.js to process file and fields upload.

--- models/Post.js
+++ models/Post.js
@@ -5,47 +5,49 @@ const sequelize = require("../config/connection");
 class Post extends Model {}
 
 Post.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    price: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    shipping: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    picture_url: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "user",
-        key: "id",
+   {
+      id: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         primaryKey: true,
+         autoIncrement: true,
       },
-    },
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "post",
-  }
+      title: {
+         type: DataTypes.STRING,
+         allowNull: false,
+      },
+      price: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+      },
+      shipping: {
+         type: DataTypes.BOOLEAN,
+         allowNull: false,
+      },
+      description: {
+         type: DataTypes.TEXT,
+         allowNull: false,
+      },
+      picture_url: {
+         // * picture_url (a string url)
+         // * to be included in post object.
+         type: DataTypes.STRING,
+         allowNull: false,
+      },
+      user_id: {
+         type: DataTypes.INTEGER,
+         references: {
+            model: 'user',
+            key: 'id',
+         },
+      },
+   },
+   {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post',
+   }
 );
 
 module.exports = Post;
