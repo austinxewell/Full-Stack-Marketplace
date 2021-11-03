3ee7b519fcd7f526bf5e703e612fb4efa1a9f830
AustinxEwell <austin.ewell86@gmail.com>
2021-11-03 15:34:19 -0600

    started links from post route to edit post handlebars. still getting empty values for post route

--- public/javascript/mylisting.js
+++ public/javascript/mylisting.js
@@ -1,32 +1,32 @@
-async function viewYourListingsHandler(event) {
-    event.preventDefault();
+// async function viewYourListingsHandler(event) {
+//     event.preventDefault();
 
-    const title = document.querySelector('#post-title').value.trim();
-    const price = document.querySelector('#post-price').value.trim();
-    const shipping = document.querySelector('#post-shipping').value.trim();
-    const description = document.querySelector('#post-description').value.trim();
-    const picture_url = document.querySelector('#picture_url').value.trim();
+//     const title = document.querySelector('#post-title').value.trim();
+//     const price = document.querySelector('#post-price').value.trim();
+//     const shipping = document.querySelector('#post-shipping').value.trim();
+//     const description = document.querySelector('#post-description').value.trim();
+//     const picture_url = document.querySelector('#picture_url').value.trim();
 
-    const response = await fetch(`/api/users/:id`, {
-        method: 'GET',
-        body: JSON.stringify({
-            title,
-            price,
-            shipping,
-            description,
-            picture_url,
-        }),
-        headers: {
-            'Content-Type': 'applicaton/json'
-        }
-    });
+//     const response = await fetch(`/api/users/:id`, {
+//         method: 'GET',
+//         body: JSON.stringify({
+//             title,
+//             price,
+//             shipping,
+//             description,
+//             picture_url,
+//         }),
+//         headers: {
+//             'Content-Type': 'applicaton/json'
+//         }
+//     });
 
-    if (response.ok) {
-        document.location.render('/mylisting');
-    } else {
-        alert('Your item was not submitted!');
-    }
-}
+//     if (response.ok) {
+//         document.location.render('/mylisting');
+//     } else {
+//         alert('Your item was not submitted!');
+//     }
+// }
 
 
 async function newListingFormHandler(event) {
@@ -34,30 +34,34 @@ async function newListingFormHandler(event) {
 
     const title = document.querySelector('#post-title').value.trim();
     const price = document.querySelector('#post-price').value.trim();
-    const shipping = document.querySelector('#post-shipping').value.trim();
+    // const shipping = document.querySelector('#post-shipping').value.trim();
     const description = document.querySelector('#post-description').value.trim();
-    const picture_url = document.querySelector('#picture_url').value.trim();
+    // const picture_url = document.querySelector('#picture_url').value.trim();
 
     const response = await fetch(`/api/posts`, {
         method: 'POST',
         body: JSON.stringify({
             title,
             price,
-            shipping,
+            // shipping,
             description,
-            picture_url,
+            // picture_url,
         }),
         headers: {
             'Content-Type': 'applicaton/json'
         }
     });
-
+    
     if (response.ok) {
-        document.location.replace('/dashboard');
+        // document.location.replace('/dashboard');
+        alert('passed')
+
     } else {
-        alert('Your item was not submitted!');
+        alert('failed');
+        console.log(response)
     }
 }
 
-document.querySelector('#view-your-listings').addEventListener('click', viewYourListingsHandler);
+// document.querySelector('#view-your-listings').addEventListener('click', viewYourListingsHandler);
+
 document.querySelector('#new-mylisting').addEventListener('click', newListingFormHandler);
