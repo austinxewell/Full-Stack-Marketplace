a3b370934771a9d5acd325e85e193c2ff3112c0c
AustinxEwell <austin.ewell86@gmail.com>
2021-11-02 23:08:13 -0600

    created subtract money function with demo value

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,27 +3,9 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const moneyRoutes = require('./money-routes');
-// const { User } = require('../../models');
-//const buyerReviewRoutes = require('./buyerReview-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/money', moneyRoutes);
-//router.use('/buyerReview', buyerReviewRoutes);
-
-// router.put("/money", (req,res) => {
-//     User.increment({
-//       money: 100
-//     },
-//     {
-//       where: {
-//         id: req.session.user_id
-//       }
-//     }).then(dbUserData => res.json(dbUserData))
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-//   })
 
 module.exports = router;
