b86175b4eb8bf0610ef2562183308251de7eb601
AustinxEwell <austin.ewell86@gmail.com>
2021-11-03 17:07:53 -0600

    added routes and created the link to the front end url. edited homepage to view the buyers review on button click. created all links within buyer reviews url to be functional

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,13 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
 const myListingRoutes = require('./myListing-routes');
+const buyerReviewRoutes = require('./buyer-reviews')
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 router.use('/mylisting', myListingRoutes)
+router.use('/buyer-reviews', buyerReviewRoutes)
 
 router.use((req, res) => {
     res.status(404).end();
