bd3acc71109706bd93710bc33e143e7b21d3869b
AustinxEwell <austin.ewell86@gmail.com>
2021-11-02 20:59:06 -0600

    created add money function and linked it to the front end

---
+++ controllers/api/money-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { User } = require("../../models");
+
+  router.put("/add", (req,res) => {
+    User.increment({
+      money: 100
+    },
+    {
+      where: {
+        id: req.session.user_id
+      }
+    }).then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  })
+
+  module.exports = router;
