25a06d126da6d923d36c13e07fc0a70a6d059b82
JoseEPina <jose.edpina@gmail.com>
2021-11-04 01:53:12 -0600

    Moved Cloudinary process from server to browser. Confirmed it run on Express. Testing on Heroku.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -51,53 +51,74 @@ router.get('/:id', (req, res) => {
 // CREATE a POST
 router.post('/', withAuth, (req, res) => {
    console.log('~~~INSIDE POST-ROUTES.JS  POST CREATE');
-   console.log('req.files', req.files);
-   const NO_IMAGE_URL =
-      'https://res.cloudinary.com/joseepina/image/upload/v1635966743/istockphoto-922962354-170667a_ys4tsk.jpg';
-   const file = req.files ? '/marketplace/images/' + req.files.item_img.name : ''; // GETS the file from front-end
-
-   if (file) {
-      cloudinary.uploader.upload(file, function (error, result) {
-         if (!error) {
-            Post.create({
-               title: req.body.item_title,
-               price: req.body.item_price,
-               shipping: req.body.item_shipping,
-               description: req.body.item_description,
-               picture_url: result.secure_url,
-               user_id: req.session.user_id,
-               //seller_id: req.session.user_id,
-            })
-               .then((dbPostData) => {
-                  return res.json(dbPostData);
-               })
-               .catch((err) => {
-                  console.log(err);
-                  res.status(500).json(err);
-               });
-         } else {
-            res.status(500).json({ message: 'Error in cloudinary upload.', error: error });
-         }
-      });
-   } else {
-      Post.create({
-         title: req.body.item_title,
-         price: req.body.item_price,
-         shipping: req.body.item_shipping,
-         description: req.body.item_description,
-         picture_url: NO_IMAGE_URL,
-         user_id: req.session.user_id,
-         // seller_id: req.session.user_id,
+   console.log('~ req.body', req.body);
+   Post.create({
+      title: req.body.item_title,
+      price: req.body.item_price,
+      shipping: req.body.item_shipping,
+      description: req.body.item_description,
+      picture_url: req.body.secure_url,
+      user_id: req.session.user_id,
+      //seller_id: req.session.user_id,
+   })
+      .then((dbPostData) => {
+         return res.json(dbPostData);
       })
-         .then((dbPostData) => {
-            return res.json(dbPostData);
-         })
-         .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-         });
-   }
+      .catch((err) => {
+         res.status(500).json({ message: 'Post.create() Failed.', err: err });
+      });
 });
+
+// // CREATE a POST
+// router.post('/', withAuth, (req, res) => {
+//    console.log('~~~INSIDE POST-ROUTES.JS  POST CREATE');
+//    console.log('req.files', req.files);
+//    const NO_IMAGE_URL =
+//       'https://res.cloudinary.com/joseepina/image/upload/v1635966743/istockphoto-922962354-170667a_ys4tsk.jpg';
+//    const file = req.files ? '/marketplace/images/' + req.files.item_img.name : ''; // GETS the file from front-end
+
+//    if (file) {
+//       cloudinary.uploader.upload(file, function (error, result) {
+//          if (!error) {
+//             Post.create({
+//                title: req.body.item_title,
+//                price: req.body.item_price,
+//                shipping: req.body.item_shipping,
+//                description: req.body.item_description,
+//                picture_url: result.secure_url,
+//                user_id: req.session.user_id,
+//                //seller_id: req.session.user_id,
+//             })
+//                .then((dbPostData) => {
+//                   return res.json(dbPostData);
+//                })
+//                .catch((err) => {
+//                   console.log(err);
+//                   res.status(500).json(err);
+//                });
+//          } else {
+//             res.status(500).json({ message: 'Error in cloudinary upload.', error: error });
+//          }
+//       });
+//    } else {
+//       Post.create({
+//          title: req.body.item_title,
+//          price: req.body.item_price,
+//          shipping: req.body.item_shipping,
+//          description: req.body.item_description,
+//          picture_url: NO_IMAGE_URL,
+//          user_id: req.session.user_id,
+//          // seller_id: req.session.user_id,
+//       })
+//          .then((dbPostData) => {
+//             return res.json(dbPostData);
+//          })
+//          .catch((err) => {
+//             console.log(err);
+//             res.status(500).json(err);
+//          });
+//    }
+// });
 
 router.put('/:id', withAuth, (req, res) => {
    Post.update(
--- public/javascript/mylisting.js
+++ public/javascript/mylisting.js
@@ -1,58 +1,103 @@
-async function newListingFormHandler(event) {
-   event.preventDefault();
-
-   console.log('INSIDE LISTENER ---- ');
-
-   const form = document.querySelector('.new-post-form');
-   const formData = new FormData(form);
-
-   console.log('item_img: ', formData.get('item_img'));
-   const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: formData,
-   });
-   if (response.ok) {
-      document.location.replace('/mylisting');
-   } else {
-      alert('Your item was not submitted!');
-   }
-}
-
-document.querySelector('.new-post-form').addEventListener('submit', newListingFormHandler);
-
 // async function newListingFormHandler(event) {
-//     event.preventDefault();
+//    event.preventDefault();
 
-//     const title = document.querySelector('#post-title').value.trim();
-//     const price = document.querySelector('#post-price').value.trim();
-//     // const shipping = document.querySelector('#post-shipping').value.trim();
-//     const description = document.querySelector('#post-description').value.trim();
-//     // const picture_url = document.querySelector('#picture_url').value.trim();
+//    console.log('INSIDE LISTENER ---- ');
 
-//     const response = await fetch(`/api/posts`, {
-//         method: 'POST',
-//         body: JSON.stringify({
-//             title,
-//             price,
-//             // shipping,
-//             description,
-//             // picture_url,
-//         }),
-//         headers: {
-//             'Content-Type': 'applicaton/json'
-//         }
-//     });
+//    const form = document.querySelector('.new-post-form');
+//    const formData = new FormData(form);
 
-//     if (response.ok) {
-//         // document.location.replace('/dashboard');
-//         alert('passed')
-
-//     } else {
-//         alert('failed');
-//         console.log(response)
-//     }
+//    console.log('item_img: ', formData.get('item_img'));
+//    const response = await fetch(`/api/posts`, {
+//       method: 'POST',
+//       body: formData,
+//    });
+//    if (response.ok) {
+//       document.location.replace('/mylisting');
+//    } else {
+//       alert('Your item was not submitted!');
+//    }
 // }
 
-// // document.querySelector('#view-your-listings').addEventListener('click', viewYourListingsHandler);
+// document.querySelector('.new-post-form').addEventListener('submit', newListingFormHandler);
+// *************************************************************************
+const url = 'https://api.cloudinary.com/v1_1/joseepina/image/upload';
 
-// document.querySelector('#new-mylisting').addEventListener('click', newListingFormHandler);
+const form = document.querySelector('.new-post-form');
+const NO_IMAGE_URL =
+   'https://res.cloudinary.com/joseepina/image/upload/v1635966743/istockphoto-922962354-170667a_ys4tsk.jpg';
+
+form.addEventListener('submit', (event) => {
+   event.preventDefault();
+
+   //const files = document.querySelector('#item_img').files;
+   const file = document.querySelector('#item_img').files[0];
+   console.log('~ file', file);
+
+   if (file) {
+      const formFileData = new FormData();
+
+      formFileData.append('file', file);
+      formFileData.append('upload_preset', 'project-2');
+
+      fetch(url, {
+         method: 'POST',
+         body: formFileData,
+      })
+         .then((response) => {
+            return response.json();
+         })
+         .then((data) => {
+            console.log('INSIDE LISTENER ---- ');
+
+            // const form = document.querySelector('.new-post-form');
+            const formData = new FormData(form);
+            formData.append('secure_url', data.secure_url);
+            console.log('~ secure_url', data.secure_url);
+
+            console.log('~~~ Form Data. Item Title: ', formData.get('item_title'));
+            console.log('~~~ Form Data. Item Price: ', formData.get('item_price'));
+            console.log('~~~ Form Data. Item Shipping: ', formData.get('item_shipping'));
+            console.log('~~~ Form Data. Item Description: ', formData.get('item_description'));
+            console.log('~~~ Form Data. Item URL: ', formData.get('secure_url'));
+            fetch(`/api/posts`, {
+               method: 'POST',
+               body: formData,
+            }).then((response) => {
+               if (response.ok) {
+                  document.location.replace('/mylisting');
+               } else {
+                  alert('Your item was not submitted!');
+               }
+            });
+
+            // const response = fetch(`/api/posts`, {
+            //    method: 'POST',
+            //    body: formData,
+            // });
+            // if (response.ok) {
+            //    document.location.replace('/mylisting');
+            // } else {
+            //    alert('Your item was not submitted!');
+            // }
+         });
+   } else {
+      console.log('INSIDE LISTENER ---- ');
+
+      // const form = document.querySelector('.new-post-form');
+      const formData = new FormData(form);
+      formData.append('secure_url', NO_IMAGE_URL);
+      console.log('~ secure_url', NO_IMAGE_URL);
+
+      console.log('item_img: ', formData.get('item_img'));
+      fetch(`/api/posts`, {
+         method: 'POST',
+         body: formData,
+      }).then((response) => {
+         if (response.ok) {
+            document.location.replace('/mylisting');
+         } else {
+            alert('Your item was not submitted!');
+         }
+      });
+   }
+});
