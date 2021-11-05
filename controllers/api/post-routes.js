a7b76b3ef0596353bf55968bde00f1a0c646589c
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-11-05 05:26:12 -0600

    fixed merge issue

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -45,45 +45,19 @@ router.get('/:id', (req, res) => {
       });
 });
 
-// CREATE a POST
-router.post('/', withAuth, (req, res) =>
-{
-   const NO_IMAGE_URL =
-      'https://res.cloudinary.com/joseepina/image/upload/v1635966743/istockphoto-922962354-170667a_ys4tsk.jpg';
-   const file = req.files ? '/marketplace/images/' + req.files.item_img.name : ''; // GETS the file from front-end
-
-   if (file) {
-      cloudinary.uploader.upload(file, function (error, result) {
-         if (!error) {
-            Post.create({
-               title: req.body.item_title,
-               price: req.body.item_price,
-               shipping: req.body.item_shipping,
-               description: req.body.item_description,
-               picture_url: result.secure_url,
-               sellers_id: req.session.user_id,
-               //seller_id: req.session.user_id,
-            })
-               .then((dbPostData) => {
-                  return res.json(dbPostData);
-               })
-               .catch((err) => {
-                  console.log(err);
-                  res.status(500).json(err);
-               });
-         } else {
-            res.status(500).json({ message: 'Error in cloudinary upload.', error: error });
-         }
-      });
-   } else {
-      Post.create({
-         title: req.body.item_title,
-         price: req.body.item_price,
-         shipping: req.body.item_shipping,
-         description: req.body.item_description,
-         picture_url: NO_IMAGE_URL,
-         sellers_id: req.session.user_id,
-         // seller_id: req.session.user_id,
+// CREATE a POST with Cloudinary Data
+router.post('/', withAuth, (req, res) => {
+   Post.create({
+      title: req.body.item_title,
+      price: req.body.item_price,
+      shipping: req.body.item_shipping,
+      description: req.body.item_description,
+      picture_url: req.body.secure_url,
+      sellers__id: req.session.user_id,
+      //seller_id: req.session.user_id,
+   })
+      .then((dbPostData) => {
+         return res.json(dbPostData);
       })
       .catch((err) => {
          res.status(500).json({ message: 'Post.create() Failed.', err: err });
