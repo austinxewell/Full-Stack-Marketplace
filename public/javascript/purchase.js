d424057b1e2e203b10a88b5495a120739dfddaf4
JoseEPina <jose.edpina@gmail.com>
2021-11-05 18:09:47 -0600

    Hotfix for Purchase Button to include newly generated buttons (after new listing is dynamically added.)

--- public/javascript/purchase.js
+++ public/javascript/purchase.js
@@ -1,17 +1,20 @@
 async function subtractMoney(event) {
-    event.preventDefault();
-    
-    const response = await fetch(`/api/money/subtract`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' }
-    });
+   event.preventDefault();
+   console.log('subtractMoney is clicked');
 
-    if (response.ok) {
-        alert('We have subtracted 35 from your account!');
-    } else {
-        // alert(response.statusText);
-        alert('You must be logged in in order to purchase an item. Please log in or create an account.')
-    }
-};
+   const response = await fetch(`/api/money/subtract`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+   });
 
-document.querySelector('#purchase').addEventListener('click', subtractMoney);
+   if (response.ok) {
+      alert('We have subtracted 35 from your account!');
+   } else {
+      // alert(response.statusText);
+      alert('You must be logged in in order to purchase an item. Please log in or create an account.');
+   }
+}
+
+document.querySelectorAll('.purchase').forEach((button) => {
+   button.addEventListener('click', subtractMoney);
+});
