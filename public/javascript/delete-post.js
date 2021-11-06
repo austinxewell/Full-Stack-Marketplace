3e0b05e2e797f412523b165386eb2f80cc1b22d0
JoseEPina <jose.edpina@gmail.com>
2021-11-06 01:32:38 -0600

    Working on DELETE Route for posts/listings showing in Dashboard.

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -2,7 +2,8 @@ async function deleteFormHandler(event) {
    event.preventDefault();
    console.log('deleteFormHandler is clicked');
 
-   //const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
+   const id = this.post.id;
+   console.log('This is ID:', id);
    const response = await fetch(`/api/post/${id}`, {
       method: 'DELETE',
    });
@@ -14,6 +15,17 @@ async function deleteFormHandler(event) {
    }
 }
 
-document.querySelector('.delete').forEach((button) => {
+document.querySelectorAll('#delete').forEach((button) => {
    button.addEventListener('click', deleteFormHandler);
 });
+
+// * EXAMPLE CODE TO EXPLORE *
+// *var allSubjectName = document.querySelectorAll('.subjectName');
+// *for (var index = 0; index < allSubjectName.length; index++) {
+// *   allSubjectName[index].addEventListener('click', function () {
+// *      this.classList.toggle('active');
+// *   });
+// *   allSubjectName[index].querySelector('button').addEventListener('click', function () {
+// *      this.closest('.subjectName').remove();
+// *   });
+// * }
